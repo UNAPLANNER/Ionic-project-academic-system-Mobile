7bf3c084b451950e59a1e58bb1658ea83ad5ac1e
RECV <134547520+RECV00@users.noreply.github.com>
2026-05-24 23:20:10 -0600

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDf7p81RogGmx85Ag1ee1szV5J6ffl0O0',
+    authDomain: 'academic-system-una.firebaseapp.com',
+    projectId: 'academic-system-una',
+    storageBucket: 'academic-system-una.firebasestorage.app',
+    messagingSenderId: '473029646366',
+    appId: '1:473029646366:web:600ec84d4ccf7889c7f60f'
+  }
 };
